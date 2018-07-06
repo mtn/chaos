00b834f608cc0dbe8ac34d6dd2b74e867af84fa9
Michael Noronha <michaeltnoronha@gmail.com>
2018-07-06 07:31:23 -0700

    Enable cargo test

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 #![feature(panic_implementation)]
 #![feature(core_intrinsics)]
 #![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
+#[cfg(test)]
+extern crate std;
 extern crate volatile;
 #[macro_use]
 extern crate lazy_static;
 extern crate spin;
+#[cfg(test)]
+extern crate array_init;
 
 use core::panic::PanicInfo;
 
 #[macro_use]
 mod vga_buffer;
 
+#[cfg(not(test))]
 #[panic_implementation]
 #[no_mangle]
 fn panic(info: &PanicInfo) -> ! {
@@ -20,6 +26,7 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello world{}", "!");
